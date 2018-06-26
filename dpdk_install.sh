81bcd7d94da081759c741d9bfd83e4f22de34b39
Colin Zhang <6488835+realzhangm@users.noreply.github.com>
2018-06-26 12:29:06 +0800

    Update dpdk_install.sh

--- dpdk_install.sh
+++ dpdk_install.sh
@@ -1,29 +1,42 @@
 #!/bin/sh
-set -e
-
 kPWD=`pwd`
 kUpDir=`dirname $kPWD`
 kOutPutDir=local
 kRTE_SDK_TARGET=x86_64-native-linuxapp-gcc
 
 vFLAG_BUILD_DPDK=0
-vFLAG_MLX=0
-vDPDK_CONFIG=" CONFIG_RTE_KNI_KMOD=n CONFIG_RTE_LIBRTE_KNI=n "
-vDPDK_VERSON="dpdk-stable-16.11.7"
+vFLAG_MLX4=0
+vFLAG_MLX5=0
+vFLAG_KNI=0
+vDPDK_CONFIG=""
+vDPDK_VERSON="16.11.7"
 
-down_dpdk()
+declare -A vNameMap=(["18.05"]="dpdk-18.05" \
+                    ["18.02.2"]="dpdk-stable-18.02.2" \
+                    ["17.11.3"]="dpdk-stable-17.11.3" \
+                    ["16.11.7"]="dpdk-stable-16.11.7")
+
+down_dpdk_all()
 {
-    dpdk_list="dpdk-18.05 dpdk-16.11.7 dpdk-17.11.3 dpdk-18.02.2"  
-    mkdir -p $kUpDir/vendors
-
-    for i in $dpdk_list; do
-        if [[ ! -f $kUpDir/vendors/$i.tar.xz ]]; then
-            wget https://fast.dpdk.org/rel/$i.tar.xz -O  $kUpDir/vendors/$i.tar.xz
-        fi
-        tar -xf $kUpDir/vendors/$i.tar.xz -C $kUpDir/vendors
+    for i in ${!vNameMap[@]}; do
+        down_dpdk $i
     done
 }
 
+down_dpdk()
+{
+    mkdir -p $kUpDir/vendors
+    ver=$1
+
+    if [[ ! -f $kUpDir/vendors/dpdk-$ver.tar.xz ]]; then
+        wget https://fast.dpdk.org/rel/dpdk-$ver.tar.xz -O  $kUpDir/vendors/dpdk-$ver.tar.xz
+    fi
+
+    if [[ ! -d $kUpDir/vendors/dpdk-$ver ]]; then
+        tar -xf $kUpDir/vendors/dpdk-$ver.tar.xz -C $kUpDir/vendors
+        mv $kUpDir/vendors/${vNameMap[$ver]}  $kUpDir/vendors/dpdk-$ver
+    fi
+}
 
 mk_dpdk_dir()
 {
@@ -34,13 +47,13 @@ mk_dpdk_dir()
 
     rm -rf ./dpdk
 
-    if [[ ! -d $kUpDir/vendors/$dpdk_version ]]; then
-        echo "download first !!!";exit 1;
-    else
-        ln -s $kUpDir/vendors/$dpdk_version dpdk 
+    if [[ ! -d $kUpDir/vendors/dpdk-$dpdk_version ]]; then
+        echo "download first, dpdk-$dpdk_version!!!"
+        down_dpdk $dpdk_version
     fi
-}
 
+    ln -s $kUpDir/vendors/dpdk-$dpdk_version dpdk 
+}
 
 build_dpdk()
 {
@@ -57,44 +70,44 @@ parse_args()
     do
         length=${#arg}
         if [[ $length -gt $"3" ]]; then
-            substr1=${arg:0:4}
-            substr2=${arg:4}
+            str1=${arg:0:4}
+            str2=${arg:4}
 
-            vFLAG_BUILD_DPDK=1
-
-            if [[ "x"$substr2 == "x18.05" ]]; then
-                vDPDK_VERSON="dpdk-18.05"
-
-            elif [[ "x"$substr2 == "x18.02.2" ]]; then
-                vDPDK_VERSON="dpdk-stable-18.02.2"
-
-            elif [[ "x"$substr2 == "x17.11.3" ]]; then
-                vDPDK_VERSON="dpdk-stable-17.11.3"
-
-            elif [[ "x"$substr2 == "x16.11.7" ]]; then
-                vDPDK_VERSON="dpdk-stable-16.11.7"
-
-            else
-                vDPDK_VERSON="dpdk-stable-16.11.7"
+            if [[ "x$str1" == "xdpdk" ]]; then
+                vFLAG_BUILD_DPDK=1
+                if [ ! -z "${vNameMap[$str2]}" ]; then
+                    vDPDK_VERSON=$str2
+                fi
             fi
         fi
        
-        if [ $arg == "mlx" ]; then    
+        if [ $arg == "mlx4" ]; then    
             vFLAG_MLX=1
             vFLAG_BUILD_DPDK=1
-        fi        
+            vDPDK_CONFIG+=" CONFIG_RTE_LIBRTE_MLX4_PMD=y "
+        fi          
+
+        if [ $arg == "mlx5" ]; then    
+            vFLAG_MLX=1
+            vFLAG_BUILD_DPDK=1
+            vDPDK_CONFIG+=" CONFIG_RTE_LIBRTE_MLX5_PMD=y "
+        fi          
+
+        if [ $arg == "kni" ]; then    
+            vFLAG_KNI=1
+            vFLAG_BUILD_DPDK=1
+        fi  
 
         if [ $arg == "download" ]; then    
-            down_dpdk;exit 0;
+            down_dpdk_all;exit 0;
         fi
-        
     done
 }
 
 build()
 {
-    if [[ $vFLAG_MLX == 1 ]]; then
-        vDPDK_CONFIG+=" CONFIG_RTE_LIBRTE_MLX4_PMD=y CONFIG_RTE_LIBRTE_MLX5_PMD=y "
+    if [[ $vFLAG_KNI==0 ]]; then
+        vDPDK_CONFIG+=" CONFIG_RTE_KNI_KMOD=n CONFIG_RTE_LIBRTE_KNI=n "
     fi
 
     if [[ $vFLAG_BUILD_DPDK == 1 ]]; then
